f01be09f32101f3b41a98274b1873b7a107f3ecb
Sergey Sedelnikov <sergey.sedelnikov@altal.fi>
2016-03-26 16:27:18 +0200

    files added

---
+++ src/components/map.js
@@ -0,0 +1,10 @@
+export class Map{
+
+  attached(){
+    //initialize map access
+    L.mapbox.accessToken = 'pk.eyJ1Ijoic2VyZ2V5c2VkZWxuaWtvdiIsImEiOiJjaW05NzZucDEwMDBnd2RtOGo0N3U4YTJ4In0.BnhUwye9rhjh9z2124wkQA';
+    var map = L.mapbox.map('map', 'mapbox.streets')
+        .setView([57.15000, 65.53333], 12);
+  }
+
+}
---
+++ src/elements/navBar.js
@@ -0,0 +1,10 @@
+import {bindable} from 'aurelia-framework';
+
+export class NavBar{
+  @bindable router;
+
+  constructor(){
+
+  }
+
+}
