277fa2fd1825d96750104ae8b3214391bfc69f09
Sergey Sedelnikov <sergey.sedelnikov@altal.fi>
2016-03-27 20:10:03 +0300

    some map features

--- src/components/map.js
+++ src/components/map.js
@@ -6,36 +6,61 @@ export class Map{
     this.self = this;
     self.points = [{
       pos: [57.153525, 65.514422],
-      color: "#295EF0",
+      color: "#49ACF2",
       marker: 'park'
     },
     {
       pos: [57.145169, 65.577218],
-      color: "#FCEE28",
+      color: "#49ACF2",
       marker: 'park2'
     },
     {
       pos: [57.148405, 65.567143],
-      color: "#FCEE28",
+      color: "#49ACF2",
       marker: 'monument'
     },
     {
       pos: [57.133828, 65.550730],
-      color: "#FCEE28",
+      color: "#F53155",
       marker: 'building'
     },
     {
       pos: [57.144173, 65.526712],
-      color: "#FCEE28",
+      color: "#49ACF2",
       marker: 'theatre'
     },
     {
       pos: [57.134690, 65.561341],
-      color: "#F030B6",
+      color: "#F53155",
       marker: 'building'
-    }]
+    }];
   }
 
+  //fetching markers from the backend and putting them on the map
+  fetchMarkers(map){
+    //set up clustering
+    var markers = new L.MarkerClusterGroup();
+    _.forEach(self.points, a=>{
+        var marker = L.marker(new L.LatLng(a.pos[0], a.pos[1]), {
+            icon: L.mapbox.marker.icon(
+              {
+                'marker-symbol': a.marker,
+                'marker-color': a.color,
+                'marker-size': 'large'
+              }),
+            title: 'index'
+        });
+        marker.on('mouseover', (e)=>{
+          e.target.openPopup();
+        });
+        marker.bindPopup('<a href="/#/ghghghg">hello google</a>');
+        markers.addLayer(marker);
+    });
+
+    map.addLayer(markers);
+  }
+
+  //on attached to DOM
   attached(){
     //initialize map access
     L.mapbox.accessToken = 'pk.eyJ1Ijoic2VyZ2V5c2VkZWxuaWtvdiIsImEiOiJjaW05NzZucDEwMDBnd2RtOGo0N3U4YTJ4In0.BnhUwye9rhjh9z2124wkQA';
@@ -43,19 +68,8 @@ export class Map{
         .map('map', 'mapbox.streets')
         .setView([57.15000, 65.53333], 12);
 
-    //set up clustering
-    var markers = new L.MarkerClusterGroup();
-    _.forEach(self.points, a=>{
-        var title = a[2];
-        var marker = L.marker(new L.LatLng(a.pos[0], a.pos[1]), {
-            icon: L.mapbox.marker.icon({'marker-symbol': a.marker, 'marker-color': a.color}),
-            title: 'index'
-        });
-        marker.bindPopup(title);
-        markers.addLayer(marker);
-    });
 
-    self.map.addLayer(markers);
+    //start fetching markers
+    this.fetchMarkers(self.map);
   }
-
 }
